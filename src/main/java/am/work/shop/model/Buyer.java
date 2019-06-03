795782bf20c553f0a44a088c71dea6ddc80f069b
Davit Karchikyan <davit.karchikyan@iunetworks.am>
2019-06-03 16:03:49 +0400

    add buyer and seller models

---
+++ src/main/java/am/work/shop/model/Buyer.java
@@ -0,0 +1,21 @@
+package am.work.shop.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Buyer implements Serializable {
+
+    private static final long serialVersionUID = -7048973251009022242L;
+
+    private String firmName;
+    private String buyerName;
+    private String buyerLastName;
+    private String buyerPhone;
+    private String buyerAddress;
+    private String buyerMail;
+}
---
+++ src/main/java/am/work/shop/model/Seller.java
@@ -0,0 +1,21 @@
+package am.work.shop.model;
+
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Seller implements Serializable {
+
+    private static final long serialVersionUID = -6437451603861439288L;
+
+    private String firmName;
+    private String sellerName;
+    private String sellerLastName;
+    private String sellerPhone;
+    private String sellerAddress;
+    private String sellerMail;
+}
